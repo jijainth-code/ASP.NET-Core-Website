abd31da1ff36b2e57d89220c1d1faeb02b2c4be9
jijainth-code <78716846+jijainth-code@users.noreply.github.com>
2022-10-27 20:50:11 +0200

    Add files via upload

---
+++ dbo.customer.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[customer] (
+    [Id]     INT            IDENTITY (1, 1) NOT NULL,
+    [Name]   NVARCHAR (MAX) NOT NULL,
+    [amount] INT            NULL,
+    CONSTRAINT [PK_customer] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
